45a0e3419beba40734a3d3f2123963183d94bae1
Justin Menestrina <jmenestr@gmail.com>
2015-07-30 11:51:12 -0700

    Refactored rpn_calculator excercise

--- w2/w2d1/exercises/lib/00_rpn_calculator.rb
+++ w2/w2d1/exercises/lib/00_rpn_calculator.rb
@@ -11,27 +11,19 @@ class RPNCalculator
   end
 
   def plus
-    calc_error?
-    nums = @stack.pop(2)
-    @stack.push(nums[0] + nums[1])
+    operation(:+)
   end
 
   def minus
-    calc_error?
-    nums = @stack.pop(2)
-    @stack.push(nums[0] - nums[1])
+    operation(:-)
   end
 
   def times
-    calc_error?
-    nums = @stack.pop(2)
-    @stack.push(nums[0]*nums[1])
+    operation(:*)
   end
 
   def divide
-    calc_error?
-    nums = @stack.pop(2)
-    @stack.push(nums[0]/nums[1])
+    operation(:/)
   end
   
   def value
@@ -39,44 +31,45 @@ class RPNCalculator
   end
 
   def operation(op_sym)
-    operators = [:+,:-,:*,:/]
+
+    calc_error?
     case op_sym
       when :+
-        plus
+       nums = @stack.pop(2)
+       @stack.push(nums[0] + nums[1])
       when :-
-        minus
+       nums = @stack.pop(2)
+       @stack.push(nums[0]-nums[1])
       when :*
-        times
+      nums = @stack.pop(2)
+       @stack.push(nums[0]*nums[1])
       when :/
-        divide
+       nums = @stack.pop(2)
+       @stack.push(nums[0]/nums[1])
     end
 
   end
 
-  def evaluate(expression) #evalues a given expression in RPN form
-    tokens(expression)#split expression into array (use helper function)
-    while @operands.length > 0 #interate through split expression
-      if @operands[0].is_a?(Numeric)#if index is a number, push to stack
-        @stack.push(@operands[0])
-        @operands.slice!(0)
-      else #if index is an operator
-        oper = @operands[0]
-        @operands.slice!(0)
-        operation(oper) #pass correct operation to plus/times/divide/minus methods
+  def evaluate(string) #evalues a given expression in RPN form
+    expression = tokens(string)#split expression into array (use helper function)
+
+    expression.each do |char|
+      if operator?(char)
+        operation(char)
+      else
+        @stack << char
       end
     end
     value
   end
 
-  def tokens(expression)
-    @operands = expression.split(" ")
-    @operands.each_with_index { |item,index|
-      if is_digit?(item)
-        @operands[index] = item.to_f
-      else
-        @operands[index] = item.to_sym
-      end
-    }
+  def tokens(string)
+    expression = string.split(" ")
+    expression.map {|char| operator?(char) ? char.to_sym : Float(char)}
+  end
+
+  def operator?(char)
+    Operators.include?(char.to_s.to_sym)
   end
 
   def calc_error?
