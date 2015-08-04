225b8a33aa69ba45b8584a5987fa600ed6d17794
Justin Menestrina <jmenestr@gmail.com>
2015-08-03 17:33:40 -0700

    Finished maze solver

--- w3/bonus/mazes/maze_solver.rb
+++ w3/bonus/mazes/maze_solver.rb
@@ -1,6 +1,6 @@
 require 'byebug'
 class MazeSolver
-  attr_accessor :grid, :cells
+  attr_accessor :grid, :cells, :open_cells, :closed_cells, :path
   attr_reader :height, :width, :start, :finish
 
   def initialize(maze)
@@ -18,7 +18,36 @@ class MazeSolver
     @open_cells = [@start]
     @closed_cells = []
     @path = []
-    
+  end
+
+  def solve
+    while open_cells.length > 0
+      #print open_cells
+      #byebug
+      open_cells.sort_by! {|cell| cell.f}
+      cell = open_cells.shift
+      closed_cells << cell
+      if cell == finish
+        return get_parents
+        break
+        #display maze
+      end
+      get_adjacent_cells(cell).each do |adj_cell|
+        
+        if adj_cell.reachable and !closed_cells.include?(adj_cell)
+
+          #If adjacent cell in open list, check to see if path is shortest
+          if open_cells.include?(adj_cell)
+            if adj_cell.g > (cell.g + 10)
+              update_cell(cell,adj_cell)
+            end
+          else
+            update_cell(cell,adj_cell)
+            open_cells << adj_cell
+          end
+        end
+      end
+    end
   end
 
   def [](pos)
@@ -32,13 +61,12 @@ class MazeSolver
   end
 
   def to_s
-    
     display = grid.map {|row| row.join("")}
     display.join("\n")
   end
 
   def get_cell(row,col)
-    @cells.select {|cell| cell.row == row and cell.column == col}
+    @cells.find {|cell| cell.row == row and cell.column == col}
   end
 
   def get_hueristic(cell)
@@ -49,7 +77,7 @@ class MazeSolver
     cells = []
     #up
     up = [cell.row-1,cell.column]
-    cells.push(get_cell(*up)) if inside_maze?(up)
+    cells << get_cell(*up) if inside_maze?(up)
     #down
     down = [cell.row+1,cell.column]
     cells << get_cell(*down) if inside_maze?(down)
@@ -60,6 +88,14 @@ class MazeSolver
     left = [cell.row, cell.column - 1]
     cells << get_cell(*left) if inside_maze?(left)
     cells
+  end
+
+  def get_parents
+    cell = finish
+    while cell.parent != start
+      cell = cell.parent
+      self[[cell.row, cell.column]] = "X"  
+    end   
   end
 
   def inside_maze?(pos)
@@ -97,17 +133,24 @@ class MazeSolver
 
     grid.each_with_index do |row,i|
       row.each_with_index do |space,j|
-        reachable = self[[i,j]] == "*" ? false : true
-        @cells << Cell.new(i,j,reachable)
+        @cells << Cell.new(i,j,reachable?([i,j]))
       end
     end
     
   end
 
+  def reachable?(pos)
+    if self[pos] == "+" or self[pos] == "-" or self[pos] == "|"
+      return false
+    end
+    true
+  end
+
 end
 
 class Cell
-  attr_reader :row,:column,:reachable
+  attr_reader :row,:column,:reachable, :f
+  attr_accessor :g, :h, :parent
   def initialize(row,column,reachable)
     @row = row
     @column = column
@@ -115,7 +158,7 @@ class Cell
     @g = 0
     @h = 0
     @f = 0
-    @rechable = reachable
+    @reachable = reachable
   end
 
   def calculate_f
@@ -126,4 +169,5 @@ end
 
 
 x = MazeSolver.new("maze1.txt")
-x.cells
+x.solve
+puts x.to_s
