cf6a7b20b4b71e84faa62831a944402cba6dafea
Justin Menestrina <jmenestr@gmail.com>
2015-08-04 23:09:17 -0700

    Added Article model

---
+++ w4/blog_app/app/controllers/articles_controller.rb
@@ -0,0 +1,39 @@
+class ArticlesController < ApplicationController
+  include ArticlesHelper
+
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to @article
+  end
+
+  def destroy
+    Article.find(params[:id]).destroy
+    flash.notice = "Article Destroyed"
+    redirect_to articles_path
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+    flash.notice = "Article '#{@article.title}' updated"
+    redirect_to @article
+  end
+
+end
